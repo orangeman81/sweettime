2f795a3654d61138c3f64312d13f2c8c2f115aaa
stefano.imparato <stefano.imparato@innovaway.it>
2019-04-12 17:38:43 +0200

    suite time

--- app/ui.js
+++ app/ui.js
@@ -1,5 +1,35 @@
 "use strict";
 
-window.onload = function () {
+// navbarToggle
+var menu = document.querySelector("#menu");
+var nav = document.querySelector("#mainNavbar");
+menu.onclick = function () {
+    if (nav.classList.contains("open")) {
+        nav.classList.remove("open");
+    } else {
+        nav.classList.add("open");
+    }
+}
 
+// carousel
+var slideIndex = [1, 1, 1, 1];
+var slideId = ["slider1", "slider2", "slider3", "slider4"]
+showDivs(1, 0);
+showDivs(1, 1);
+showDivs(1, 2);
+showDivs(1, 3);
+
+function plusDivs(n, no) {
+    showDivs(slideIndex[no] += n, no);
+}
+
+function showDivs(n, no) {
+    var i;
+    var x = document.getElementsByClassName(slideId[no]);
+    if (n > x.length) { slideIndex[no] = 1 }
+    if (n < 1) { slideIndex[no] = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex[no] - 1].style.display = "block";
 }
